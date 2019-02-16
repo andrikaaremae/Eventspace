6921797b579df09c623148db9823730005de14d7
Artjom Jakovlev <edelmoedig@gmail.com>
2019-02-16 03:00:55 +0200

    Added project base

---
+++ src/main/java/ee/ttu/eventspace/EventspaceApplication.java
@@ -0,0 +1,14 @@
+package ee.ttu.eventspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventspaceApplication.class, args);
+	}
+
+}
+
