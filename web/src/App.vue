5e1c36ed503898b20c434dacd9c6892e2edad130
andri <andrikaaremae@gmail.com>
2019-02-19 13:57:34 +0200

    Front page header design

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <router-link :to="{ name: 'HelloWorld' }">Home</router-link>
-    <router-link to="/about">About</router-link>
+      <ul>
+        <li class="home"><router-link class="router" to="/about">About</router-link></li>
+        <li><router-link class="router" :to="{ name: 'HelloWorld' }">Home</router-link></li>
+      </ul>
     <router-view/>
   </div>
 </template>
@@ -13,12 +15,49 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0;
 }
+
+  .router {
+    color: whitesmoke;
+    font-family: arial;
+    align-content: center;
+    font-size: large;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  li {
+    float: right;
+  }
+
+  .router {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  .home {
+    margin-right: 30px;
+  }
+
+  /* Change the link color to #111 (black) on hover */
+  .router:hover {
+    background-color: #111;
+  }
+
 </style>
