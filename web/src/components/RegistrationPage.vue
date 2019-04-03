7dbd78883ee75750bfbd48e35d3d5f1ba915cc61
Artjom Jakovlev <edelmoedig@gmail.com>
2019-04-03 23:14:29 +0300

    Added redirect after successful registration

--- web/src/components/RegistrationPage.vue
+++ web/src/components/RegistrationPage.vue
@@ -67,6 +67,7 @@ export default {
       }).then(response => {
         alert('User successfully registered!')
         console.log(response)
+        this.$router.push('/')
       }).catch(error => {
         alert('There has been a problem.')
         console.log(error)
