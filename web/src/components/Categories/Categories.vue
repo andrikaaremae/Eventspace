e98c01baf72b269188ff4a46fa94fb84d6069b94
andri <andrikaaremae@gmail.com>
2019-04-19 13:00:34 +0300

    Changed HelloWorld.vue name. Deleted a useless test file. Components into folders and changed TestController name.

--- web/src/components/Categories/Categories.vue
+++ web/src/components/Categories/Categories.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="categories">
-    <div class="filter">
-      <input id="locationInput" type="text" name="myLocation" placeholder="Choose location"/>
-      <input id="timeInput" type="date" name="myTime" placeholder="When?"/>
-      <input id="peopleInput" type="number" name="myPeople" placeholder="How many people?"/>
-      <input id="priceInput" type="number" name="myPrice" placeholder="Price per hour"/>
-    </div>
     <PlaceBoxes v-for="place in places" v-if="place.category==category" v-bind="place" v-bind:key="place.id"></PlaceBoxes>
   </div>
 </template>
