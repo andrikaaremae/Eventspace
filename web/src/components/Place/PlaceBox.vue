ce9fd62202cd0e3f71952c97a44709551bb98cd3
reimo <reimootter@gmail.com>
2019-05-02 17:27:38 +0300

    Merge branch 'master' of C:\Users\reimo\Desktop\Eventspace with conflicts.

--- web/src/components/Place/PlaceBox.vue
+++ web/src/components/Place/PlaceBox.vue
@@ -1,30 +1,44 @@
 <template>
+  <html>
+  <head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+  <body>
   <div class="place">
-
-      <img :src="imageURL" />
-
+    <img :src="imageURL" />
     <section class="infoSection">
-      <div class="name" align="left"><h1>{{ name }}</h1></div>
       <section>
-        <span class="address"  style="float:left">{{ address.country }}, {{ address.state}}, {{ address.city}}, {{ address.street}}, {{ address.houseNumber }}</span>
-        <span class="rating"><star-rating :show-rating="false" :star-size="20" :rating="this.ratingToShow" :read-only="true" :increment="1"></star-rating></span>
+        <div class="name" align="left"><h1>{{ name }}</h1></div>
       </section>
-
+      <aside>
+        <span class="rating"><star-rating :show-rating="false" :star-size="20" :rating="this.ratingToShow" :read-only="true" :increment="1"></star-rating></span>
+      </aside>
+      <section>
+        <div class="address" style="float:left">
+          <i class="material-icons" style="float:left">place</i>
+          {{ address.country }},
+          {{ address.state}},
+          {{ address.city}} ,
+          {{ address.street}},
+          {{ address.houseNumber }}
+        </div>
+      </section>
       <section class="infoSection">
+        <hr class="hr">
         <div class="description" align="left"><h3>About the Space</h3>{{ description }}</div>
         <div class="category" align="left"><h3>Available for </h3>{{ category }}</div>
         <div class="price" align="left"><h3>Price </h3>{{price}}€ one night</div>
         <div align="left">
-        <form method="post" @submit.prevent="addRating"><h4>Rate your stay</h4>
-          <star-rating star-size="20" onclick="submit" v-model="rating"></star-rating>
-          <button class="placeButton">{{statusText}}</button>
-        </form>
+          <form method="post" @submit.prevent="addRating"><h4>Rate your stay</h4>
+            <star-rating star-size="20" onclick="submit" v-model="rating"></star-rating>
+            <button class="placeButton">{{statusText}}</button>
+          </form>
         </div>
       </section>
       <section>
         <div v-if="username==owner.username" align="right">
-        <button v-on:click="deletePlace"  class="placeButton">Delete</button>
-        <router-link  class="placeButton" :to="{ name: 'Edit', query: {id: id}} ">Edit</router-link>
+          <button v-on:click="deletePlace"  class="placeButton">Delete</button>
+          <router-link  class="placeButton" :to="{ name: 'Edit', query: {id: id}} ">Edit</router-link>
         </div>
       </section>
     </section>
@@ -32,138 +46,179 @@
       <h1>Book your stay</h1>
       <hr>
       <section>
-      <div class="bookingBox"><BookingBox v-for="booking in bookings" v-bind="booking" v-bind:key="booking.id">></BookingBox></div>
-      <div>
-        <form method="post" @submit.prevent="addBooking">
-          <label>
-            Start Date:
-            <input type="date" v-model="startDate" required>
-          </label>
-          <br>
-          <label>
-            End Date:
-            <input type="date" v-model="endDate" required>
-          </label>
-          <br>
-          <button class="placeButton">Add booking</button>
-        </form>
-      </div>
+        <div class="bookingBox"><BookingBox v-for="booking in bookings" v-bind="booking" v-bind:key="booking.id">></BookingBox></div>
+        <hr class="hr">
+        <div>
+          <form method="post" @submit.prevent="addBooking">
+            <label>
+              Start Date:
+              <input type="date" v-model="startDate" required>
+            </label>
+            <br>
+            <label>
+              End Date:
+              <input type="date" v-model="endDate" required>
+            </label>
+            <br>
+            <button class="placeButton" style="float:left">Add booking</button>
+          </form>
+        </div>
       </section>
     </aside>
   </div>
+  </body>
+  </html>
 </template>
 <script>
-import StarRating from 'vue-star-rating'
-import BookingBox from '../BookingBox'
-import axios from 'axios'
-import authHeader from '../../services/auth-header'
-import moment from 'moment'
+  import StarRating from 'vue-star-rating'
+  import BookingBox from '../BookingBox'
+  import axios from 'axios'
+  import authHeader from '../../services/auth-header'
+  import moment from 'moment'
 
-export default {
-  name: 'App',
-  components: {
-    BookingBox,
-    StarRating},
-  data () {
-    return {
-      owner: null,
-      startDate: null,
-      endDate: null,
-      customer: null,
-      rating: null,
-      ratingToShow: null,
-      place: '',
-      ratingList: [0],
-      statusText: 'Add rating',
-      username: '',
-
-    }
-  },
-  props: ['id', 'name', 'description', 'address', 'category', 'bookings', 'price', 'imageURL', 'owner', 'username'],
-  mounted () {
-    axios.get(process.env.API_URL + '/places/get/' + this.id).then(response => {
-      this.place = response.data
-      this.ratingList = this.place.ratingList
-      console.log(this.ratingList)
-      if (this.ratingList && this.ratingList.length) {
-        let total = 0
-        for (let i = 0; i < this.ratingList.length; i++) {
-          total += this.ratingList[i]
+  export default {
+    name: 'App',
+    components: {
+      BookingBox,
+      StarRating},
+    data () {
+      return {
+        owner: null,
+        startDate: null,
+        endDate: null,
+        customer: null,
+        rating: null,
+        ratingToShow: null,
+        place: '',
+        ratingList: [0],
+        statusText: 'Add rating',
+        username: '',
+        startDateTest:'',
+        endDateTest:'',
+        dateListTest:[]
+      }
+    },
+    props: ['id', 'name', 'description', 'address', 'category', 'bookings', 'price', 'imageURL', 'owner', 'username', 'bookingList'],
+    mounted () {
+      axios.get(process.env.API_URL + '/places/get/' + this.id).then(response => {
+        this.place = response.data
+        this.ratingList = this.place.ratingList
+        console.log(this.ratingList)
+        if (this.ratingList && this.ratingList.length) {
+          let total = 0
+          for (let i = 0; i < this.ratingList.length; i++) {
+            total += this.ratingList[i]
+          }
+          let avg = total / this.ratingList.length
+          this.ratingToShow = avg.toFixed(0)
+        } else {
+          this.ratingToShow = 0
         }
-        let avg = total / this.ratingList.length
-        this.ratingToShow = avg.toFixed(0)
-      } else {
-        this.ratingToShow = 0
-      }
-    })
-    axios.get(process.env.API_URL + '/user/username').then(response =>
-    { this.username = response.data, axios.get(process.env.API_URL + '/user/getUser/'+this.username)
-      .then(response => { this.customer = response.data},);});
-  },
-  methods: {
-
-    addBooking () {
-      let startDate = moment(this.startDate, "YYYY-MM-DD");
-      let endDate = moment(this.endDate, "YYYY-MM-DD");
-      let duration = moment.duration(endDate.diff(startDate));
-      let days = duration.asDays();
-      let bookedDays;
-      if (days == 0) {
-         bookedDays = 1;
-      }else {
-         bookedDays = days;
-      }
-      let today = new Date();
-      let dd = today.getDate();
-      let mm = today.getMonth()+1;
-      let yyyy = today.getFullYear();
-      if(dd<10) {dd='0'+dd;}
-      if(mm<10) {mm='0'+mm;}
-      let now = yyyy+'-'+mm+'-'+dd;
-
-      if (this.endDate < this.startDate ) {
-        return alert('Start date can not be greater than end date!') }
-      if (this.startDate < now  ) {
-        return alert('Start date must be equal or greater than current date!') }
-      else {
-        axios.post(process.env.API_URL + '/bookings/save/' + this.id, {
-            startDate: this.startDate,
-            endDate: this.endDate,
-            price: this.price * bookedDays,
-            customer: this.customer
-          },
-          {headers: authHeader()}).then(response => response.data).then(response => this.bookings.push(response))
-      }
+      })
+      axios.get(process.env.API_URL + '/user/username').then(response =>
+      { this.username = response.data,
+        axios.get(process.env.API_URL + '/user/getUser/'+this.username)
+          .then(response => { this.customer = response.data},);});
+      axios.get(process.env.API_URL + '/bookings/getAll/' + this.$route.query.id , { headers: authHeader() }).then(response => { this.bookingList = response.data })
     },
-    addRating () {
-      this.statusText = 'Voted!';
-      this.ratingList.push(this.rating),
-      axios.post(process.env.API_URL + '/places/edit', {
-        id: this.id,
-        name: this.name,
-        category: this.category,
-        description: this.description,
-        address: {country: this.address.country,
-          state: this.address.state,
-          city: this.address.city,
-          street: this.address.street,
-          houseNumber: this.address.houseNumber,
-          zipCode: this.address.zipCode},
-        ratingList: this.ratingList,
-        price: this.price,
-        imageURL: this.imageURL,
-        owner:this.owner,
+    methods: {
+
+      addBooking () {
+
+        // Calculating date duration as Days
+        let startDate = moment(this.startDate, "YYYY-MM-DD");
+        let endDate = moment(this.endDate, "YYYY-MM-DD");
+        let duration = moment.duration(endDate.diff(startDate));
+        let days = duration.asDays() + 1;
+        console.log(days)
+
+        // Get current date
+        let bookedDays;
+        if (days == 0) {
+          bookedDays = 1;
+        }else {
+          bookedDays = days;
+        }
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth()+1;
+        let yyyy = today.getFullYear();
+        if(dd<10) {dd='0'+dd;}
+        if(mm<10) {mm='0'+mm;}
+        let now = yyyy+'-'+mm+'-'+dd;
+
+
+        // Get date array from input values
+        let getDateArray = function(startDate, endDate) {
+          let array = new Array();
+          let date = new Date(startDate);
+          while (date <= endDate) {
+            array.push(new Date(date));
+            date.setDate(date.getDate() + 1);
+          }
+          return array;
+        };
+        let dateArray = getDateArray(startDate, endDate);
+        console.log(dateArray);
+
+
+        // Get date array from db end- and startDate
+        let i;
+        for(i=0; i< this.bookingList.length; i++){
+          this.startDateTest =  this.bookingList[i].startDate;
+          this.endDateTest =  this.bookingList[i].endDate;
+
+          let startDateTest = moment(this.startDateTest, "YYYY-MM-DD");
+          let endDateTest = moment(this.endDateTest, "YYYY-MM-DD");
+          this.dateListTest = this.dateListTest.concat(getDateArray(startDateTest,endDateTest));
+
+        }
+
+
+        if (this.endDate < this.startDate ) {
+          return alert('Start date can not be greater than end date!') }
+        if (this.startDate < now  ) {
+          return alert('Start date must be equal or greater than current date!') }
+        else {
+          axios.post(process.env.API_URL + '/bookings/save/' + this.id, {
+              startDate: this.startDate,
+              endDate: this.endDate,
+              price: this.price * bookedDays,
+              customer: this.customer
+            },
+            {headers: authHeader()}).then(response => response.data).then(response => this.bookings.push(response))
+        }
+
       },
-      {headers: {'Content-type': 'application/json'}})
-    },
+      addRating () {
+        this.statusText = 'Voted!';
+        this.ratingList.push(this.rating),
+          axios.post(process.env.API_URL + '/places/edit', {
+              id: this.id,
+              name: this.name,
+              category: this.category,
+              description: this.description,
+              address: {country: this.address.country,
+                state: this.address.state,
+                city: this.address.city,
+                street: this.address.street,
+                houseNumber: this.address.houseNumber,
+                zipCode: this.address.zipCode},
+              ratingList: this.ratingList,
+              price: this.price,
+              imageURL: this.imageURL,
+              owner:this.owner,
+            },
+            {headers: {'Content-type': 'application/json'}})
+      },
 
-    deletePlace () {
-      axios.delete(process.env.API_URL + '/places/delete/' + this.id,
-        {headers: authHeader()}).then(response => window.location.reload())
+      deletePlace () {
+        axios.delete(process.env.API_URL + '/places/delete/' + this.id,
+          {headers: authHeader()}).then(response => window.location = '/#/places')
+      }
+
     }
-
   }
-}
 </script>
 
 <style scoped>
@@ -177,7 +232,9 @@ export default {
     height: 500px;
     width: 1100px;
   }
-
+  .hr{
+    width: 150%
+  }
   .name {
     font-size: 20px;
     align:left;
@@ -213,22 +270,35 @@ export default {
     width: 63%;
   }
   aside {
-    border: 1px solid;
-    border-radius: 5px;
-    float: right;
-    margin: 0 1.5%;
-    width: 30%;
-    margin-top: 20px;
+    margin-top: 30px;
   }
   .bookingBoxAside {
     width: 400px;
+    border: 1px solid;
+    border-radius: 5px;
+    float: right;
+    /*margin: 0 1.5%;*/
+    margin-top: 30px;
   }
   .infoSection {
-    width: 50%;
+    width: 55%;
   }
   .rating {
     float: left;
     margin-top: 10px;
   }
+  .address{
+    margin-bottom: 10px;
+  }
+  label{
+    display: inline-block;
+    float: left;
+    clear: left;
+    width: 250px;
+    text-align: right;
+  }
+  input {
+    display: inline-block;
 
+  }
 </style>
